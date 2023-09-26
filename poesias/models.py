bea2859872fde3c1ac39ad57338677ae76ac89d3
Rafael Tamaki <rafaeiji91@gmail.com>
2023-09-25 22:28:29 -0300

    iniciando migrações de bd

--- poesias/models.py
+++ poesias/models.py
@@ -1,3 +1,25 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
+    
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    categorias = models.ManyToManyField(Categoria)
+    data_publicacao = models.CharField(max_length=8)
+    imagem = models.ImageField(upload_to='book_covers/')
+
+    def __str__(self):
+        return self.titulo
